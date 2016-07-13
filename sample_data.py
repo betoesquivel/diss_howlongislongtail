e86339f8f41a7e4b6c7efbe8bab92b601585e8d5
Beto Esquivel <ppbeto94@gmail.com>
2016-07-13 09:35:24 +0100

    Notebook usestagme

---
+++ sample_data.py
@@ -0,0 +1,16 @@
+#!/usr/bin/enb python
+import json
+import itertools
+
+def n_samples(sample_number=50):
+    f = open('../signalmedia-1m.jsonl')
+    return [json.loads(l.strip()) for l in itertools.islice(f, sample_number)]
+
+
+if __name__ == '__main__':
+    samples = n_samples()
+    print samples
+    print "Read {0} samples.".format(len(samples))
+
+
+
