735ecdedc7d8ee50ad7ee433c84b45e0f2e3c5ed
Beto Esquivel <ppbeto94@gmail.com>
2016-08-02 20:52:57 +0100

    Stanford parse function works when imported

--- tagged_document_parsers.py
+++ tagged_document_parsers.py
@@ -2,7 +2,6 @@
 from collections import defaultdict
 import json
 
-
 starts_new_entity = lambda prev, curr: prev != curr and curr != 'O'
 is_in_entity = lambda prev, curr: prev == curr and curr != 'O'
 is_outside_entity = lambda prev, curr: prev != curr and curr == 'O'
